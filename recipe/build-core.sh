12e936800b12d14204533468d8d805bd34c21771
Serhii Kupriienko <skupriienko@anaconda.com>
2023-03-01 13:36:11 +0200

    Fix AR and disable patchelf

--- recipe/build-core.sh
+++ recipe/build-core.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -xe
 
+# For some weird reason, ar is not picked up on linux-aarch64
+if [ $(uname -s) = "Linux" ] && [ ! -f "${BUILD_PREFIX}/bin/ar" ]; then
+    ln -s "${BUILD}-ar" "${BUILD_PREFIX}/bin/ar"
+fi
+
+
 cd python/
 export SKIP_THIRDPARTY_INSTALL=1
 "${PYTHON}" setup.py build
@@ -22,10 +28,10 @@ rm -rf "$SRC_DIR/../b-o" "$SRC_DIR/../bazel-root"
 # but this may not always be true/allowed, hence the or operation.
 rm -rf /root/.cache/bazel || true
 
-if [[ "$target_platform" == "linux-"* ]]; then
-  # Remove RUNPATH and set RPATH
-  for f in "ray/_raylet.so" "ray/core/src/ray/raylet/raylet" "ray/core/src/ray/gcs/gcs_server"; do
-    patchelf --remove-rpath $SP_DIR/$f
-    patchelf --force-rpath --add-rpath $PREFIX/lib $SP_DIR/$f
-  done
-fi
+# Remove RUNPATH and set RPATH
+# if [[ "$target_platform" == "linux-"* ]]; then
+#   for f in "ray/_raylet.so" "ray/core/src/ray/raylet/raylet" "ray/core/src/ray/gcs/gcs_server"; do
+#     patchelf --remove-rpath $SP_DIR/$f
+#     patchelf --force-rpath --add-rpath $PREFIX/lib $SP_DIR/$f
+#   done
+# fi
