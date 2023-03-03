fe1830d97f081e6a971742dffb506c6ffc5c0149
Serhii Kupriienko <skupriienko@anaconda.com>
2023-03-03 15:15:15 +0200

    Enable subpackages

--- recipe/build-core.sh
+++ recipe/build-core.sh
@@ -5,7 +5,7 @@ set -xe
 if [ $(uname -s) = "Linux" ] && [ ! -f "${BUILD_PREFIX}/bin/ar" ]; then
     ln -s "${BUILD}-ar" "${BUILD_PREFIX}/bin/ar"
     ln -s "$RANLIB" "${BUILD_PREFIX}/bin/ranlib"
-    ln -sf $(which $LD) "${BUILD_PREFIX}/bin/ld"
+    ln -sf "$LD" "${BUILD_PREFIX}/bin/ld"
 fi
 
 cd python/
