57fa46790f4ed6384176e8d593e803639445d736
Mohamed Sentissi <msentissi@anaconda.com>
2024-11-18 17:14:24 -0500

    fix bazel paths for cleanup and shutdown on win

--- recipe/build-core.bat
+++ recipe/build-core.bat
@@ -12,9 +12,7 @@ echo calling pip install
 echo ==========================================================
 
 rem avoid multiple jobs causing file-access conflicts
-cat ..\.bazelrc
 echo build --jobs=1 >> ..\.bazelrc
-cat ..\.bazelrc
 "%PYTHON%" -m pip install . --no-deps --no-build-isolation
 rem remember the return code
 set RETCODE=%ERRORLEVEL%
@@ -23,14 +21,12 @@ rem Now clean everything up so subsequent builds (for potentially
 rem different Python version) do not stumble on some after-effects.
 "%PYTHON%" setup.py clean --all
 
+rem setup.py uses D:\bazel-root and D:\b-o since ray 2.10.0.
+rem Get the drive for SRC_DIR
+@for %%G in  ("%SRC_DIR%") DO @SET DRIVE=%%~dG
 rem Now shut down Bazel server, otherwise Windows would not allow moving a directory with it
-rem Path testing
-ls "%SRC_DIR%\..\bazel-root"
-ls "%SRC_DIR%\..\b-o"
-ls "%SRC_DIR%\bazel-root"
-ls "%SRC_DIR%\b-o"
-bazel "--output_user_root=%SRC_DIR%\..\bazel-root" "--output_base=%SRC_DIR%\..\b-o" clean --expunge
-bazel "--output_user_root=%SRC_DIR%\..\bazel-root" "--output_base=%SRC_DIR%\..\b-o" shutdown
-rd /s /q "%SRC_DIR%\..\b-o" "%SRC_DIR%\..\bazel-root"
+bazel "--output_user_root=%DRIVE%\bazel-root" "--output_base=%DRIVE%\b-o" clean --expunge
+bazel "--output_user_root=%DRIVE%\bazel-root" "--output_base=%DRIVE%\b-o" shutdown
+rd /s /q "%DRIVE%\b-o" "%DRIVE%\bazel-root"
 rem Ignore "bazel shutdown" errors
 exit /b %RETCODE%
