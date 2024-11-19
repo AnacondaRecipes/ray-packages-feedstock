2891064c62fa2f45dcc02881042bb37f663930ba
Mohamed Sentissi <msentissi@anaconda.com>
2024-11-19 16:25:17 -0500

    add py3.12 and win comments

--- recipe/build-core.bat
+++ recipe/build-core.bat
@@ -22,6 +22,8 @@ rem different Python version) do not stumble on some after-effects.
 "%PYTHON%" setup.py clean --all
 
 rem setup.py uses D:\bazel-root and D:\b-o since ray 2.10.0.
+rem We may have to patch this behavior out since these directories
+rem could be conceivably used (and cleaned!) concurrently by unrelated builds
 rem Get the drive for SRC_DIR
 @for %%G in  ("%SRC_DIR%") DO @SET DRIVE=%%~dG
 rem Now shut down Bazel server, otherwise Windows would not allow moving a directory with it
