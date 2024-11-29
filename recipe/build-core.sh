1682800109ec84dcde18090892e665115876501c
Mohamed Sentissi <msentissi@anaconda.com>
2024-11-29 16:28:43 -0500

    remove sudo commands

--- recipe/build-core.sh
+++ recipe/build-core.sh
@@ -14,8 +14,8 @@ fi
 
 if [[ "${target_platform}" == osx-* ]]; then
   # Increase max file handles limit to prevent "too many file handles" error during testing
-  sudo launchctl limit maxfiles 256 unlimited
-  sudo launchctl limit maxfiles 128000 524288
+  #sudo launchctl limit maxfiles 256 unlimited
+  #sudo launchctl limit maxfiles 128000 524288
   launchctl limit
   sysctl -a | grep files
   ulimit -n
